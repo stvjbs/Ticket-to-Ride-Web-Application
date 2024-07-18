0d6fe80519f1e5f42d77f30c1d87d1c2f6f79473
stvjbs <yarashukartem@icloud.com>
2024-07-19 01:05:20 +0200

    ticketService: initial commit

--- src/main/java/com/andersen/Ticket_to_Ride_Web_Application/entity/Ticket.java
+++ src/main/java/com/andersen/Ticket_to_Ride_Web_Application/entity/Ticket.java
@@ -1,20 +1,37 @@
 package com.andersen.Ticket_to_Ride_Web_Application.entity;
 
 import com.andersen.Ticket_to_Ride_Web_Application.entity.enums.ResultTicket;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
-import java.math.BigDecimal;
-import java.util.Currency;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "tickets")
+@Data
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @JoinColumn
+    @ManyToOne
     private Traveller traveller;
+    @JoinColumn
+    @ManyToOne
     private Station departure;
+    @JoinColumn
+    @ManyToOne
     private Station arrival;
+    @Column(name = "segments")
     private Integer segments;
-    private BigDecimal price;
-    private Currency currency;
+    @Column(name = "result")
+    @Enumerated(EnumType.STRING)
     private ResultTicket resultTicket;
-
 }
